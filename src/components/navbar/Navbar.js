340b375d0c109d1d82c3e5f7ca889bc068d54e5b
Kenny <keotero99@gmail.com>
2025-05-03 14:44:32 -0400

    Fixing navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '.navbar.css';
+
+import React, { useEffect, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './navbar.css';
 
 function Navbar() {
     const location = useLocation();
@@ -20,26 +21,14 @@ function Navbar() {
         });
     }, [location]);
 
-    // Toggle sidebar
-    useEffect(() => {
-        const toggle = toggleRef.current;
-        const navbar = navbarRef.current;
-
-        const handleToggle = () => {
-            navbar.classList.toggle('expander');
-        };
-
-        toggle?.addEventListener('click', handleToggle);
-
-        return () => {
-            toggle?.removeEventListener('click', handleToggle);
-        };
-    }, []);
-
     // Close sidebar when clicking outside
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (navbarRef.current && !navbarRef.current.contains(event.target) && !toggleRef.current.contains(event.target)) {
+            if (
+                navbarRef.current &&
+                (!navbarRef.current.contains(event.target)) &&
+                (!toggleRef.current || !toggleRef.current.contains(event.target))
+            ) {
                 navbarRef.current.classList.remove('expander');
             }
         };
@@ -49,33 +38,35 @@ function Navbar() {
     }, []);
 
   return (
-    <div className="l-navbar" id="navbar">
-        <nav className="nav">
-            <div>
-                <div className="nav__brand">
-                    <ion-icon name="menu-outline" class="nav__toggle" id="nav-toggle"></ion-icon>
-                    <Link to="/" className="nav__logo">Fleet Status</Link>
+    <div id='sideMenu'>
+        <div className="l-navbar" id="navbar" ref={navbarRef}>
+            <nav className="nav">
+                <div>
+                    <div className="nav__brand">
+                        <ion-icon name="menu-outline" className="nav__toggle" id="nav-toggle" onClick={() => navbarRef.current?.classList.toggle('expander')} />
+                        <Link to="/" className="nav__logo">Fleet Status</Link>
+                    </div>
+                    <div className="nav__list">
+                        <Link to="/" className="nav__link active">
+                            <ion-icon name="home-outline" className="nav__icon"></ion-icon>
+                            <span className="nav__name">Fleet Overview</span>
+                        </Link>
+                        <Link to="/AircraftStatus" className="nav__link hidden" id="aircraftStatusPage">
+                            <ion-icon name="airplane-outline" className="nav__icon"></ion-icon>
+                            <span className="nav__name" id="aircraftStatusText">Aircraft Status</span>
+                        </Link>
+                        <Link to="/History" className="nav__link">
+                            <ion-icon name="reader-outline" className="nav__icon"></ion-icon>
+                            <span className="nav__name">History</span>
+                        </Link>
+                    </div>
                 </div>
-                <div className="nav__list">
-                    <Link to="/" className="nav__link active">
-                        <ion-icon name="home-outline" class="nav__icon"></ion-icon>
-                        <span className="nav__name">Fleet Overview</span>
-                    </Link>
-                    <Link to="/AircraftStatus" className="nav__link hidden" id="aircraftStatusPage">
-                        <ion-icon name="airplane-outline" class="nav__icon"></ion-icon>
-                        <span className="nav__name" id="aircraftStatusText">Aircraft Status</span>
-                    </Link>
-                    <Link to="/History" className="nav__link">
-                        <ion-icon name="reader-outline" class="nav__icon"></ion-icon>
-                        <span className="nav__name">History</span>
-                    </Link>
-                </div>
-            </div>
-            <Link to="/login" className="nav__link" id="loginStatus">
-                <ion-icon name="log-out-outline" class="nav__icon"></ion-icon>
-                <span className="nav__name" id="loginStatusText">Sign In</span>
-            </Link>
-        </nav>
+                <Link to="/login" className="nav__link" id="loginStatus">
+                    <ion-icon name="log-out-outline" className="nav__icon"></ion-icon>
+                    <span className="nav__name" id="loginStatusText">Sign In</span>
+                </Link>
+            </nav>
+        </div>
     </div>
   );
 }
---
+++ src/pages/AircraftStatus.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AircraftStatus() {
+    return(
+        <div></div>
+    );
+}
+
+export default AircraftStatus;
